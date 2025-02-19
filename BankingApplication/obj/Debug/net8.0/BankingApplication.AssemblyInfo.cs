3360b7482eb92ec92c5a76770cb394f123c47e09
tekkyd <tekderek123@gmail.com>
2025-02-19 17:42:18 +1100

    Added spacing in between nav links

--- BankingApplication/obj/Debug/net8.0/BankingApplication.AssemblyInfo.cs
+++ BankingApplication/obj/Debug/net8.0/BankingApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e5f36c97d67c6c600f0a8bf02ecd3dc1a1002ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e16937a9a333126248bb85b46fe9b43c4303078")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
