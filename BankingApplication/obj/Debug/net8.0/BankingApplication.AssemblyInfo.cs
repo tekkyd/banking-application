8b7d692fb94174a8840ed30792883df242adaad8
tekkyd <tekderek123@gmail.com>
2025-02-20 21:23:53 +1100

    Added a select button on all accounts

--- BankingApplication/obj/Debug/net8.0/BankingApplication.AssemblyInfo.cs
+++ BankingApplication/obj/Debug/net8.0/BankingApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e16937a9a333126248bb85b46fe9b43c4303078")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3360b7482eb92ec92c5a76770cb394f123c47e09")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
